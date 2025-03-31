e6062ce9c3103ad077796fe85036a12fca1aa166
Arshia Joshi <arshiaamoljoshi@gmail.com>
2025-03-31 21:47:40 +0530

    Changed image for slideshow in donation

--- donation_script.js
+++ donation_script.js
@@ -1,13 +1,13 @@
-let slides = document.querySelectorAll(".slide");
-let currentIndex = 0;
+let slideIndex = 0;
+showSlides();
 
 function showSlides() {
-    slides.forEach((slide, index) => {
-        slide.classList.remove("active");
-    });
-    slides[currentIndex].classList.add("active");
-    currentIndex = (currentIndex + 1) % slides.length;
+    let slides = document.querySelectorAll(".slide");
+    slides.forEach(slide => slide.style.display = "none");
+    
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+    
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
 }
-
-showSlides(); 
-setInterval(showSlides, 3000); 
